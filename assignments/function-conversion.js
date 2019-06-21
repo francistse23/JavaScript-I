c5938114158c5a0606602a837bd7e58e170b4e68
francistse23 <wtse.ucsd@gmail.com>
2019-06-20 20:21:53 -0700

    Finished function-conversion.js

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,21 +5,39 @@
 // };
 // myFunction();
 
+myFunction = () => {
+  console.log("Function was invoked!");
+};
+myFunction();
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+anotherFunction = param => {
+  return param;
+};
+anotherFunction("Example");
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+add = (param1, param2) => {
+  return param1 + param2;
+};
+add(1, 2);
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
-
+subtract = (param1, param2) => {
+  return param1 - param2;
+};
+subtract(1, 2);
 
 // Stretch
 
@@ -28,3 +46,6 @@
 //   return num * 3;
 // });
 // console.log(triple);
+exampleArray = [1, 2, 3, 4];
+const triple = exampleArray.map(num => num * 3);
+console.log(triple);
